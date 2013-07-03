bd0dc1afe907e21431f5c989071b3c7e69de077b
jiangks <jiangks@localhost.localdomain>
2013-07-04 02:15:58 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opstest::Application.config.secret_key_base = '1343991b33b75dfc0573a4b1d1c95fca637db0d5c3e049b5df6a866ad0de1fa716ba285167825a4e6f49c4dbfbf9828ca29ecbdd01aea5b9f935836e46350cc6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Opstest::Application.config.session_store :encrypted_cookie_store, key: '_opstest_session'
